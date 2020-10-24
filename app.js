0c536a82da9d905c8382c41c715c49eee980f8a1
deepshi-devlope <64650012+deepshi-devlope@users.noreply.github.com>
2020-10-24 12:03:44 +0530

    Add files via upload
    app.js with routes

--- app.js
+++ app.js
@@ -33,3 +33,64 @@ app.use('/users',require('./routes/users'));
 
 const port=3000||process.env.PORT;
 app.listen(port,()=>console.log(`Server started on port ${port}...`))
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/login",function(req,res){
+  res.sendFile(__dirname + "/login.html");
+});
+
+app.get("/login/Manager",function(req,res){
+  res.sendFile(__dirname + "/managerLogin.html");
+});
+
+app.post("/login/Manager",function(req,res){
+  res.sendFile(__dirname + "/Manager.html");
+});
+
+app.get("/login/Employee",function(req,res){
+  res.sendFile(__dirname + "/employeeLogin.html");
+});
+
+app.post("/login/Employee",function(req,res){
+  res.sendFile(__dirname + "/Employee.html");
+});
+
+app.get("/Employee/Teammates", function(req,res){
+  res.sendFile(__dirname + "/Teammates.html");
+});
+
+app.get("/Employee/Tasks", function(req,res){
+  res.sendFile(__dirname + "/Tasks.html");
+});
+
+app.get("/Leaderboard", function(req,res){
+  res.sendFile(__dirname + "/Leaderboard.html");
+});
+
+app.get("/Employee/Rewards", function(req,res){
+  res.sendFile(__dirname + "/Rewards.html");
+});
+
+
+app.get("/Chat", function(req,res){
+  res.sendFile(__dirname + "/Chat.html");
+});
+
+app.get("/Manager/Tasks", function(req,res){
+  res.sendFile(__dirname + "/Tasks.html");
+});
+
+app.get("/Manager/Create", function(req,res){
+  res.send("Create teams");
+});
+
+app.get("/Manager/AddEmployee", function(req,res){
+  res.send("Add employee");
+});
+
+app.get("/Manager/Points", function(req,res){
+  res.send( "Assign points");
+});
